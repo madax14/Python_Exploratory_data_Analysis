6840f49e691a9e38c4b5ece2e5f859902ec74b7d
Diego Santos <120533129+madax14@users.noreply.github.com>
2024-11-16 12:49:14 -0300

    Update Babies_Name_Analysis.py

--- Babies_Name_Analysis.py
+++ Babies_Name_Analysis.py
@@ -1,6 +1,7 @@
+# Data exploratory with PYTHON and PANDAS.
+
 #!/usr/bin/env python
 # coding: utf-8
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -31,17 +32,23 @@ df["Region"].isnull()
 # To show which rows (indexes) are the null values
 df[df["Region"].isnull()].index
 
-# Understanding that in the Region table, a specific or more States are not listed in the Region table
+# Understanding about the Region table, if a specific or more States are not listed in the Region table
 df.iloc[[275663,  275664,  275665,  275666,  275667,  275668,  275669,  275670,
         275671,  275672]]
 
 # Grouping by to check if the null values are only in the MI state
-# They actually are only in MI 
+# They actually are only in the MI state, in the Region table, it was not listed.
 df.groupby("State")["Name"].count()
 # Created a variable to identify the null values
 nullValues = df["Region"].isnull()
 # Replacing the NULL values for the text "midwest" within the Region column
+# No need to create a new DF, once this code below already applies it in the DF.
 df.loc[nullValues, "Region"] = "Midwest"
+
+# Checking for duplicated values within the DF
+# No duplicated vales
+df.duplicated()
+df.duplicated().value_counts()
 
 
 
